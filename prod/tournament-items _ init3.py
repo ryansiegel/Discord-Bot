177543f5ec831ae77d4b2defc7f4fde1e69d0ffc
Ryan Siegel <75775075+ryansiegel@users.noreply.github.com>
2021-11-25 08:40:22 -0500

    Delete tournament-items _ init3.py

--- prod/tournament-items _ init3.py
+++
@@ -1,9 +0,0 @@
-#start round command
-@inter_client.slash_command(name="tournament-items", description="Items for tournament staff to use.")
-async def roundtimer(inter): 
-    pass
-@roundtimer.sub_command(name="staff-dispute", description="Allows a staff member to open a dispute for two trainers.", options=[Option("trainer1", "Trainer one in the dispute.", OptionType.USER, required=True), Option("Trainer two in the dispute.", OptionType.USER, required=True)])
-async def extendedround(inter, trainer1, trainer2):
-    reload(mod)
-    channelPrint = bot.get_channel(000000000000000000)
-    await mod.staffdispute(inter, channelPrint, bot, trainer1, trainer2)
